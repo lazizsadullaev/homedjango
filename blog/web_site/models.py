f452ba559711bc12ea3d3a859b27b26df03247d1
Kamol <sadullaevkamol2004@gmail.com>
2023-10-05 17:12:59 +0500

    added file

--- blog/web_site/models.py
+++ blog/web_site/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=155, verbose_name='Название категории')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
